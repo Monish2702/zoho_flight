2dd42c97d660a2e885075d484e679bcac60774f2
monish <gsmkumar2002@gmail.com>
2022-04-08 11:09:27 +0530

    change 3 in cancel file

--- cancellation.py
+++ cancellation.py
@@ -3,13 +3,19 @@ import json
 def cancel(): #to cancel seats for a booking id
  print("Booking_ID(s)",list(dict_list.keys()))
  print("Already booked Seat(s)",list(dict_list.values()))
+
  cancel_id=input("Enter booking id in which a seat is to be cancelled from the above ids: ")
  print(dict_list[cancel_id])
+
  cancel_seat=input("Enter index of seat number to be cancelled from the above list: ")
  print(data)
+
  del data[cancel_id]["Seat_prices"][cancel_seat]
+ #updating the data file
  with open("DictFile.json","w") as f:
      json.dump(data,f)
+
+#-----------------------------------------------------------------------
 
 def existing(): #to know the booked seats
     # print("Existing bookings: ", dict_list)
@@ -17,11 +23,14 @@ def existing(): #to know the booked seats
     flat_list = [num for sublist in existing_Seats for num in sublist]
     # print(flat_list)
     return flat_list
+
+#-----------------------------------------------------------------------
+
 d={}
 l=[]
 l1=[]
 tup1=()
-with open("DictFile.json","r") as file:
+with open("DictFile.json","r") as file: #extracting only the booking id and seat locations
     data = json.load(file)
     for key,value in data.items():
         l1.append(key)
